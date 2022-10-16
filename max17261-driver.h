13d1671f12355234f276c99facf15f54b0bb51ff
Iwan <iwangre@gmail.com>
2022-10-16 16:45:15 +0200

    Initial commit

---
+++ max17261-driver.h
@@ -0,0 +1,42 @@
+/////////////////////////////////////
+///  MAX17261 Driver for ESPHome ///
+///     By Adagio_Leopard       ///
+///    www.hailthebean.com     ///
+/////////////////////////////////
+
+#include "esphome.h"
+
+class cMax17261 : public PollingComponent, public Sensor 
+{
+
+private:
+int getBatterySOC()
+{
+}
+
+bool getChDc()
+{
+}
+
+
+public:
+
+///constructor
+cMax17261() : PollingComponent(1000){}
+
+float get_setup_priority() const override { return esphome::setup_priority::BUS; }
+
+void setup() override
+{
+	
+}
+
+void update() override
+{
+	void update() override
+	{
+		publish_state(42.0);
+	}
+}
+	
+}
